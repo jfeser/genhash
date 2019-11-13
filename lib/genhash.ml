40160ff1c9666989b4a50b393e567505883db8c9
Jack Feser <feser@csail.mit.edu>
2019-11-13 16:41:56 -0500

    Bug in generation of b.

--- lib/genhash.ml
+++ lib/genhash.ml
@@ -8,7 +8,7 @@ let gen_multiply_shift ?(max_time = Time.Span.millisecond) ?(seed = 0) keys =
   let rand = Random.State.make [| seed |] in
   let log_bins = Int.ceil_log2 (List.length keys) in
   let nbins = Int.(2 ** log_bins) in
-  let max_b = Int.(2 ** (63 - nbins)) in
+  let max_b = Int.(2 ** (63 - log_bins)) in
   let collider = Array.create ~len:nbins 0 in
   let start_time = Time.now () in
   let rec loop () =
@@ -41,4 +41,10 @@ let gen_multiply_shift ?(max_time = Time.Span.millisecond) ?(seed = 0) keys =
 let%expect_test "" =
   gen_multiply_shift [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10 ]
   |> [%sexp_of: t option] |> print_s;
-  [%expect {| (((a 2497643567980153265) (b 78436873956136) (log_bins 4))) |}]
+  [%expect
+    {| (((a 2497643567980153265) (b 541917767041968936) (log_bins 4))) |}]
+
+let%expect_test "" =
+  gen_multiply_shift (List.init 100 ~f:Fun.id)
+  |> [%sexp_of: t option] |> print_s;
+  [%expect {| (((a 3849860515398355207) (b 69325505536368030) (log_bins 7))) |}]
