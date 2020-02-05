bcbfdc57aeefca105bcf426d97e416b746343793
Jack Feser <feser@csail.mit.edu>
2020-02-05 16:05:20 -0500

    Changes.

--- lib/genhash.ml
+++ lib/genhash.ml
@@ -2,9 +2,17 @@ open! Core
 
 type t = { a : int; b : int; log_bins : int } [@@deriving compare, sexp]
 
-let hash { a; b; log_bins } v = ((a * v) + b) lsr (63 - log_bins)
+let hash { a; b; log_bins } v =
+  let a = Unsigned.UInt64.of_int a in
+  let b = Unsigned.UInt64.of_int b in
+  let v = Unsigned.UInt64.of_int v in
+  Unsigned.UInt64.Infix.(((a * v) + b) lsr Int.(64 - log_bins))
+  |> Unsigned.UInt64.to_int
 
 let gen_hash ?(max_time = Time.Span.millisecond) ?(seed = 0) ?log_bins keys =
+  if List.contains_dup ~compare:[%compare: int] keys then
+    failwith "List contains duplicates.";
+
   let rand = Random.State.make [| seed |] in
   let log_bins =
     match log_bins with Some n -> n | None -> Int.ceil_log2 (List.length keys)
@@ -14,7 +22,7 @@ let gen_hash ?(max_time = Time.Span.millisecond) ?(seed = 0) ?log_bins keys =
   let collider = Array.create ~len:nbins 0 in
   let start_time = Time.now () in
   let rec loop () =
-    if Time.diff (Time.now ()) start_time > max_time then None
+    if Time.(Span.(diff (now ()) start_time > max_time)) then None
     else
       let a = Random.State.int rand Int.max_value in
       let a = if a mod 2 = 0 then a + 1 else a in
@@ -51,13 +59,13 @@ let search_hash ?seed ?max_time keys =
 let%expect_test "" =
   gen_hash [ 1; 2; 3; 4; 5; 6; 7; 8; 9; 10 ] |> [%sexp_of: t option] |> print_s;
   [%expect
-    {| (((a 2497643567980153265) (b 541917767041968936) (log_bins 4))) |}]
+    {| (((a 3341259039328860149) (b 246399585519049361) (log_bins 4))) |}]
 
 let%expect_test "" =
   gen_hash (List.init 100 ~f:Fun.id) |> [%sexp_of: t option] |> print_s;
   [%expect {| (((a 3849860515398355207) (b 69325505536368030) (log_bins 7))) |}]
 
 let%expect_test "" =
-  gen_hash ~max_time:Time.Span.minute (List.init 10000000 ~f:Fun.id)
+  gen_hash ~max_time:Time.Span.second (List.init 1000 ~f:Fun.id)
   |> [%sexp_of: t option] |> print_s;
-  [%expect {| (((a 3849860515398355207) (b 69325505536368030) (log_bins 7))) |}]
+  [%expect {| (((a 461625378747127019) (b 3649080310458621) (log_bins 10))) |}]
